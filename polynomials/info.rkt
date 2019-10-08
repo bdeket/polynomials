ac4aaa7fba84f895dd36d582128aebbe0072b52a
Bert De Ketelaere <bedeke@hotmail.com>
2019-10-08 21:21:34 +0200

    fix dependencies

--- polynomials/info.rkt
+++ polynomials/info.rkt
@@ -2,9 +2,20 @@
 
 (define collection 'multi)
 
-(define deps '("racket/base" "math-lib" "math-doc"))
-(define implies '("math-lib" "math-doc"))
-(define build-deps '("rackunit" "racket-doc"))
+(define deps '("racket/base"
+               "typed-racket-lib"
+               "math-lib"
+			   "math-doc"))
+(define implies '("math-lib"
+                  "math-doc"))
+(define build-deps '("rackunit"
+					 "rackunit-typed"
+					 
+                     "scribble-lib"
+                     "racket-doc"
+					 "typed-racket-doc"
+					 "plot-gui-lib"
+					 "sandbox-lib"))
 
 (define pkg-desc "Adds polynomials to the math collection")
 
